0c05c151566c4b51575924b62b5ae8beac86f42e
Will Prendergast <prendygast@hotmail.com>
2024-01-23 14:21:55 +0000

    Initial commit

---
+++ coal-yard-website/src/main/java/com/econfuels/coalyardwebsite/CoalYardWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.econfuels.coalyardwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoalYardWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoalYardWebsiteApplication.class, args);
+	}
+
+}
---
+++ coal-yard-website/src/test/java/com/econfuels/coalyardwebsite/CoalYardWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.econfuels.coalyardwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoalYardWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
